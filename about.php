b80678588392eabbf6d23c95cb263b60d22aae0d
Vicky-24-11 <73150251+Vicky-24-11@users.noreply.github.com>
2023-01-22 01:48:51 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,38 @@
+<?php include('./constant/layout/head.php');?>
+<?php include('./constant/layout/header.php');?>
+
+<?php include('./constant/layout/sidebar.php');?>
+
+
+<div class="page-wrapper">
+	<div class="container-fluid">
+		<div class="card">
+			<div class="card-body">
+				<div class="about-head text-center mb-5">
+					<h1 style="color: black;" class="mb-2 mt-4 f-s-50 ">Vikas Pandey</h1>
+					<span>A Student Of Model College</span>
+				</div>
+				<div class="about">
+					<div class="row">
+						<div class="col-md-3">
+							<!-- <img src="assets/myimages/profile.jpg" class="img-fluid"> -->
+						</div>
+						
+						
+						<div class="col-md-4">
+							<ul class="list-unstyled">
+								<li><span>Expert in</span> : <b>HTML , CSS , JAVASCRIPT , PHP, MySQL,</b></li>
+								<li><span>Residence</span> :  <b>India</b></li>
+								<li><span>Email</span> :  <b>vikaspandey567890@gmail.com</b></li>
+							</ul>
+						</div>
+					</div>
+					
+				</div>
+			</div>			
+		</div>
+	</div>
+</div>
+
+
+<?php include('./constant/layout/footer.php');?>
